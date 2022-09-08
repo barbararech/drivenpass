1f2c69e950054c4adee71fa34807a25a9d80d536
barbararech <rech.brb@gmail.com>
2022-09-08 10:20:27 -0300

    feat: add prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import "express-async-errors";
 // import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 
 // import cardRouter from "./routes/cardRouter.js";
 // import rechargeRouter from "./routes/rechargeRouter.js";
 // import paymentRouter from "./routes/paymentRouter.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
