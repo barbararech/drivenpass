b4b3ced403794b09b73836668cd800da3e120567
barbararech <rech.brb@gmail.com>
2022-09-08 10:11:48 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import cors from "cors";
+import "express-async-errors";
+// import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
+
+// import cardRouter from "./routes/cardRouter.js";
+// import rechargeRouter from "./routes/rechargeRouter.js";
+// import paymentRouter from "./routes/paymentRouter.js";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+// app.use(cardRouter);
+// app.use(rechargeRouter);
+// app.use(paymentRouter);
+
+// app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server On!"));
